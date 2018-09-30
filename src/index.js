0a765357d6222a624ea8639eb7a685145ad1d5f8
gokel166 <okellogeorge40@yahoo.com>
2018-09-30 18:21:37 -0500

    Set up coordinates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import '../node_modules/leaflet/dist/leaflet.css';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
